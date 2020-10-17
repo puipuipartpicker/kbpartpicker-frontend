a059a6ab57719bf039551f84af567282e22926c8
scoffrin <scoffrin@indeed.com>
2020-10-17 21:08:06 +0900

    add routing and some routs

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import Paths from './types/Paths'
+import Search from './components/Search'
 
 function App() {
   return (
     <div className="App">
       <h1>So many parts to pick from</h1>
+      <div className="App-categories">
+        <button className="App-categories-button"><Link to={Paths.cases}>Cases</Link></button>
+        <button className="App-categories-button"><Link to={Paths.pcb}>PCB</Link></button>
+        <button className="App-categories-button"><Link to={Paths.plates}>Plates</Link></button>
+        <button className="App-categories-button"><Link to={Paths.stabilizers}>Stabilizers</Link></button>
+        <button className="App-categories-button"><Link to={Paths.switches}>Switches</Link></button>
+        <button className="App-categories-button"><Link to={Paths.keycaps}>Keycaps</Link></button>
+      </div>
+  
+      <Route path="/" />
+      <Route path={Paths.cases} render={ (props) => <Search category='cases' theme='red'/> } />
+      <Route path={Paths.pcb} render={ (props) => <Search category='PCBs' theme='blue'/> } />
+      <Route path={Paths.plates} render={ (props) => <Search category='plates' theme='green'/> } />
+      <Route path={Paths.stabilizers} render={ (props) => <Search category='stabilizers' theme='yellow'/> } />
+      <Route path={Paths.switches} render={ (props) => <Search category='switches' theme='magic'/> } />
+      <Route path={Paths.keycaps} render={ (props) => <Search category='keycaps' theme='rainbow'/> } />
+
     </div>
   );
 }
---
+++ src/types/Paths.ts
@@ -0,0 +1,11 @@
+enum Paths {
+  main = '/' ,
+  switches = '/switches',
+  cases = '/cases',
+  pcb = '/pcv',
+  plates = '/plates',
+  stabilizers = '/stabilizers',
+  keycaps = '/keycaps'
+}
+
+export default Paths
