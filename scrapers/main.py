604f6bc23e542542a3bc970a0c6f81093cfb41f6
vimiomori <vivian.muchen@gmail.com>
2020-10-17 22:10:40 +0900

    Clean up switch names

--- scrapers/main.py
+++ scrapers/main.py
@@ -1,4 +1,5 @@
 # https://medium.com/@mikelcbrowne/running-chromedriver-with-python-selenium-on-heroku-acc1566d161c
+import re
 from selenium import webdriver 
 from selenium.webdriver.common.by import By 
 from selenium.webdriver.support.ui import WebDriverWait 
@@ -13,19 +14,29 @@ def main():
     cards = driver.find_elements_by_class_name("grid-view-item__link")
     i = 0
     while i < len(cards) - 1:
-        cards = driver.find_elements_by_class_name("grid-view-item__link")
-        cards[i].click()
+        card = driver.find_elements_by_class_name("grid-view-item__link")[i]
+        product = card.find_element_by_class_name("visually-hidden").text
+        if 'Sample' in product or 'Big' in product:
+            i += 1
+            continue
+        card.click()
         try:
             types = Select(driver.find_element_by_id('SingleOptionSelector-0'))
-        # if types:
-            print([o.text for o in types.options])
+            name = driver.find_element_by_class_name("product-single__title").text
+            name = re.sub(r' Switches', '', name)
+            for j, o in enumerate(types.options):
+                if j == 0:
+                    continue
+                else:
+                    print(f"{name} {o.text}")
+
         except:
-        #     pass
-        # else:
-            print(driver.find_element_by_class_name("product-single__title").text)
+            name = driver.find_element_by_class_name("product-single__title").text
+            name = re.sub(r' Switches', '', name)
+            print(name)
         i += 1
         driver.back()
-        
+    driver.close()
 
 
 if __name__ == "__main__":
