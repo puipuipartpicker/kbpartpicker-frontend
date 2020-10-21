6fff745721ed8b4b9b5db5bad9a2774c52e74a2d
scoffrin <scoffrin@indeed.com>
2020-10-21 12:17:19 +0900

    product on search results made sticky and more test data

--- frontend/src/TestData.tsx
+++ frontend/src/TestData.tsx
@@ -18,6 +18,206 @@ export const searchResults = [{
       name: 'KBDfans', 
       url: 'https://kbdfans.com/', 
       imgURL: 'https://cdn.shopify.com/s/files/1/1473/3902/files/new-logo_02851fbb-7bd7-46f4-a6ba-86485d041224_200x@2x.png?v=1581588430'}
+  },{
+    id: 123,
+    name: 'test name',
+    imgURL: 'https://cdn.shopify.com/s/files/1/3099/8088/products/SpaceCadetII_1080x.png?v=1601649358',
+    stock: true,
+    priceRange: '$45 ~ $48',
+    vendor: {
+      name: 'NovelKeys_', 
+      url: 'https://novelkeys.xyz/', 
+      imgURL: 'https://cdn.shopify.com/s/files/1/3099/8088/files/Logo_a8cf9d15-aa0b-42ab-8176-2942f169d7da_720x.png?v=1565381568'}
+  }, {
+    id: 666,
+    name: 'number of the beef',
+    imgURL: 'https://cdn.shopify.com/s/files/1/3099/8088/products/NK65PCB-Front_1024x1024@2x.png?v=1596543025',
+    stock: false,
+    priceRange: '$75 ~ $110',
+    vendor: {
+      name: 'KBDfans', 
+      url: 'https://kbdfans.com/', 
+      imgURL: 'https://cdn.shopify.com/s/files/1/1473/3902/files/new-logo_02851fbb-7bd7-46f4-a6ba-86485d041224_200x@2x.png?v=1581588430'}
+  },{
+    id: 123,
+    name: 'test name',
+    imgURL: 'https://cdn.shopify.com/s/files/1/3099/8088/products/SpaceCadetII_1080x.png?v=1601649358',
+    stock: true,
+    priceRange: '$45 ~ $48',
+    vendor: {
+      name: 'NovelKeys_', 
+      url: 'https://novelkeys.xyz/', 
+      imgURL: 'https://cdn.shopify.com/s/files/1/3099/8088/files/Logo_a8cf9d15-aa0b-42ab-8176-2942f169d7da_720x.png?v=1565381568'}
+  }, {
+    id: 666,
+    name: 'number of the beef',
+    imgURL: 'https://cdn.shopify.com/s/files/1/3099/8088/products/NK65PCB-Front_1024x1024@2x.png?v=1596543025',
+    stock: false,
+    priceRange: '$75 ~ $110',
+    vendor: {
+      name: 'KBDfans', 
+      url: 'https://kbdfans.com/', 
+      imgURL: 'https://cdn.shopify.com/s/files/1/1473/3902/files/new-logo_02851fbb-7bd7-46f4-a6ba-86485d041224_200x@2x.png?v=1581588430'}
+  },{
+    id: 123,
+    name: 'test name',
+    imgURL: 'https://cdn.shopify.com/s/files/1/3099/8088/products/SpaceCadetII_1080x.png?v=1601649358',
+    stock: true,
+    priceRange: '$45 ~ $48',
+    vendor: {
+      name: 'NovelKeys_', 
+      url: 'https://novelkeys.xyz/', 
+      imgURL: 'https://cdn.shopify.com/s/files/1/3099/8088/files/Logo_a8cf9d15-aa0b-42ab-8176-2942f169d7da_720x.png?v=1565381568'}
+  }, {
+    id: 666,
+    name: 'number of the beef',
+    imgURL: 'https://cdn.shopify.com/s/files/1/3099/8088/products/NK65PCB-Front_1024x1024@2x.png?v=1596543025',
+    stock: false,
+    priceRange: '$75 ~ $110',
+    vendor: {
+      name: 'KBDfans', 
+      url: 'https://kbdfans.com/', 
+      imgURL: 'https://cdn.shopify.com/s/files/1/1473/3902/files/new-logo_02851fbb-7bd7-46f4-a6ba-86485d041224_200x@2x.png?v=1581588430'}
+  },{
+    id: 123,
+    name: 'test name',
+    imgURL: 'https://cdn.shopify.com/s/files/1/3099/8088/products/SpaceCadetII_1080x.png?v=1601649358',
+    stock: true,
+    priceRange: '$45 ~ $48',
+    vendor: {
+      name: 'NovelKeys_', 
+      url: 'https://novelkeys.xyz/', 
+      imgURL: 'https://cdn.shopify.com/s/files/1/3099/8088/files/Logo_a8cf9d15-aa0b-42ab-8176-2942f169d7da_720x.png?v=1565381568'}
+  }, {
+    id: 666,
+    name: 'number of the beef',
+    imgURL: 'https://cdn.shopify.com/s/files/1/3099/8088/products/NK65PCB-Front_1024x1024@2x.png?v=1596543025',
+    stock: false,
+    priceRange: '$75 ~ $110',
+    vendor: {
+      name: 'KBDfans', 
+      url: 'https://kbdfans.com/', 
+      imgURL: 'https://cdn.shopify.com/s/files/1/1473/3902/files/new-logo_02851fbb-7bd7-46f4-a6ba-86485d041224_200x@2x.png?v=1581588430'}
+  },{
+    id: 123,
+    name: 'test name',
+    imgURL: 'https://cdn.shopify.com/s/files/1/3099/8088/products/SpaceCadetII_1080x.png?v=1601649358',
+    stock: true,
+    priceRange: '$45 ~ $48',
+    vendor: {
+      name: 'NovelKeys_', 
+      url: 'https://novelkeys.xyz/', 
+      imgURL: 'https://cdn.shopify.com/s/files/1/3099/8088/files/Logo_a8cf9d15-aa0b-42ab-8176-2942f169d7da_720x.png?v=1565381568'}
+  }, {
+    id: 666,
+    name: 'number of the beef',
+    imgURL: 'https://cdn.shopify.com/s/files/1/3099/8088/products/NK65PCB-Front_1024x1024@2x.png?v=1596543025',
+    stock: false,
+    priceRange: '$75 ~ $110',
+    vendor: {
+      name: 'KBDfans', 
+      url: 'https://kbdfans.com/', 
+      imgURL: 'https://cdn.shopify.com/s/files/1/1473/3902/files/new-logo_02851fbb-7bd7-46f4-a6ba-86485d041224_200x@2x.png?v=1581588430'}
+  },{
+    id: 123,
+    name: 'test name',
+    imgURL: 'https://cdn.shopify.com/s/files/1/3099/8088/products/SpaceCadetII_1080x.png?v=1601649358',
+    stock: true,
+    priceRange: '$45 ~ $48',
+    vendor: {
+      name: 'NovelKeys_', 
+      url: 'https://novelkeys.xyz/', 
+      imgURL: 'https://cdn.shopify.com/s/files/1/3099/8088/files/Logo_a8cf9d15-aa0b-42ab-8176-2942f169d7da_720x.png?v=1565381568'}
+  }, {
+    id: 666,
+    name: 'number of the beef',
+    imgURL: 'https://cdn.shopify.com/s/files/1/3099/8088/products/NK65PCB-Front_1024x1024@2x.png?v=1596543025',
+    stock: false,
+    priceRange: '$75 ~ $110',
+    vendor: {
+      name: 'KBDfans', 
+      url: 'https://kbdfans.com/', 
+      imgURL: 'https://cdn.shopify.com/s/files/1/1473/3902/files/new-logo_02851fbb-7bd7-46f4-a6ba-86485d041224_200x@2x.png?v=1581588430'}
+  },{
+    id: 123,
+    name: 'test name',
+    imgURL: 'https://cdn.shopify.com/s/files/1/3099/8088/products/SpaceCadetII_1080x.png?v=1601649358',
+    stock: true,
+    priceRange: '$45 ~ $48',
+    vendor: {
+      name: 'NovelKeys_', 
+      url: 'https://novelkeys.xyz/', 
+      imgURL: 'https://cdn.shopify.com/s/files/1/3099/8088/files/Logo_a8cf9d15-aa0b-42ab-8176-2942f169d7da_720x.png?v=1565381568'}
+  }, {
+    id: 666,
+    name: 'number of the beef',
+    imgURL: 'https://cdn.shopify.com/s/files/1/3099/8088/products/NK65PCB-Front_1024x1024@2x.png?v=1596543025',
+    stock: false,
+    priceRange: '$75 ~ $110',
+    vendor: {
+      name: 'KBDfans', 
+      url: 'https://kbdfans.com/', 
+      imgURL: 'https://cdn.shopify.com/s/files/1/1473/3902/files/new-logo_02851fbb-7bd7-46f4-a6ba-86485d041224_200x@2x.png?v=1581588430'}
+  },{
+    id: 123,
+    name: 'test name',
+    imgURL: 'https://cdn.shopify.com/s/files/1/3099/8088/products/SpaceCadetII_1080x.png?v=1601649358',
+    stock: true,
+    priceRange: '$45 ~ $48',
+    vendor: {
+      name: 'NovelKeys_', 
+      url: 'https://novelkeys.xyz/', 
+      imgURL: 'https://cdn.shopify.com/s/files/1/3099/8088/files/Logo_a8cf9d15-aa0b-42ab-8176-2942f169d7da_720x.png?v=1565381568'}
+  }, {
+    id: 666,
+    name: 'number of the beef',
+    imgURL: 'https://cdn.shopify.com/s/files/1/3099/8088/products/NK65PCB-Front_1024x1024@2x.png?v=1596543025',
+    stock: false,
+    priceRange: '$75 ~ $110',
+    vendor: {
+      name: 'KBDfans', 
+      url: 'https://kbdfans.com/', 
+      imgURL: 'https://cdn.shopify.com/s/files/1/1473/3902/files/new-logo_02851fbb-7bd7-46f4-a6ba-86485d041224_200x@2x.png?v=1581588430'}
+  },{
+    id: 123,
+    name: 'test name',
+    imgURL: 'https://cdn.shopify.com/s/files/1/3099/8088/products/SpaceCadetII_1080x.png?v=1601649358',
+    stock: true,
+    priceRange: '$45 ~ $48',
+    vendor: {
+      name: 'NovelKeys_', 
+      url: 'https://novelkeys.xyz/', 
+      imgURL: 'https://cdn.shopify.com/s/files/1/3099/8088/files/Logo_a8cf9d15-aa0b-42ab-8176-2942f169d7da_720x.png?v=1565381568'}
+  }, {
+    id: 666,
+    name: 'number of the beef',
+    imgURL: 'https://cdn.shopify.com/s/files/1/3099/8088/products/NK65PCB-Front_1024x1024@2x.png?v=1596543025',
+    stock: false,
+    priceRange: '$75 ~ $110',
+    vendor: {
+      name: 'KBDfans', 
+      url: 'https://kbdfans.com/', 
+      imgURL: 'https://cdn.shopify.com/s/files/1/1473/3902/files/new-logo_02851fbb-7bd7-46f4-a6ba-86485d041224_200x@2x.png?v=1581588430'}
+  },{
+    id: 123,
+    name: 'test name',
+    imgURL: 'https://cdn.shopify.com/s/files/1/3099/8088/products/SpaceCadetII_1080x.png?v=1601649358',
+    stock: true,
+    priceRange: '$45 ~ $48',
+    vendor: {
+      name: 'NovelKeys_', 
+      url: 'https://novelkeys.xyz/', 
+      imgURL: 'https://cdn.shopify.com/s/files/1/3099/8088/files/Logo_a8cf9d15-aa0b-42ab-8176-2942f169d7da_720x.png?v=1565381568'}
+  }, {
+    id: 666,
+    name: 'number of the beef',
+    imgURL: 'https://cdn.shopify.com/s/files/1/3099/8088/products/NK65PCB-Front_1024x1024@2x.png?v=1596543025',
+    stock: false,
+    priceRange: '$75 ~ $110',
+    vendor: {
+      name: 'KBDfans', 
+      url: 'https://kbdfans.com/', 
+      imgURL: 'https://cdn.shopify.com/s/files/1/1473/3902/files/new-logo_02851fbb-7bd7-46f4-a6ba-86485d041224_200x@2x.png?v=1581588430'}
   }]
 
 
