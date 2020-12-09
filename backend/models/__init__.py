2d131deea860f638b44f4732f78e18d19f1349a5
vimiomori <vivian.muchen@gmail.com>
2020-12-09 21:23:03 +0900

    import models individually

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -1,10 +1,10 @@
-from .vendor import Vendor
-from .vendor_product_association import VendorProductAssociation
-from .product import Product
+# from .vendor import Vendor
+# from .vendor_product_association import VendorProductAssociation
+# from .product import Product
 
 
-__all__ = [
-    "Vendor",
-    "VendorProductAssociation",
-    "Product"
-]
+# __all__ = [
+#     "Vendor",
+#     "VendorProductAssociation",
+#     "Product"
+# ]
