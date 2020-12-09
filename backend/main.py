ec1e7c19c1c36930559d741bc15accb2d692ece2
vimiomori <vivian.muchen@gmail.com>
2020-12-09 20:04:37 +0900

    test with buildpack

--- backend/main.py
+++ backend/main.py
@@ -15,30 +15,30 @@ def main(session, driver):
     NovelKeys(session, driver).run()
 
 
-# if __name__ == "__main__":
-prd = os.environ.get('DATABASE_URL')    
-if prd:
-    engine = create_engine(prd)
-    session = sessionmaker(bind=engine)()
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
-else:
-    engine = create_engine('postgres+psycopg2://vi:password@localhost:5432/kbpartpicker')
-    session = sessionmaker(bind=engine)()
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument("--headless")
-    driver = webdriver.Chrome()
-timeout = 3
-try:
-    element_present = EC.presence_of_element_located((By.ID, 'main'))
-    WebDriverWait(driver, timeout).until(element_present)
-except TimeoutException:
-    print("Timed out waiting for page to load")
-finally:
-    main(session, driver)
-    driver.close()
-    session.close()
+if __name__ == "__main__":
+    prd = os.environ.get('DATABASE_URL')    
+    if prd:
+        engine = create_engine(prd)
+        session = sessionmaker(bind=engine)()
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+        driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+    else:
+        engine = create_engine('postgres+psycopg2://vi:password@localhost:5432/kbpartpicker')
+        session = sessionmaker(bind=engine)()
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument("--headless")
+        driver = webdriver.Chrome()
+    timeout = 3
+    try:
+        element_present = EC.presence_of_element_located((By.ID, 'main'))
+        WebDriverWait(driver, timeout).until(element_present)
+    except TimeoutException:
+        print("Timed out waiting for page to load")
+    finally:
+        main(session, driver)
+        driver.close()
+        session.close()
