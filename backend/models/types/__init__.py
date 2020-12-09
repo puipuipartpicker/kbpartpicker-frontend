5609500419eb495957bcba2e701af8f8e1cc29eb
vimiomori <vivian.muchen@gmail.com>
2020-12-09 22:17:30 +0900

    add types back

---
+++ backend/models/types/__init__.py
@@ -0,0 +1,15 @@
+# from .layout_type import LayoutType
+# from .product_type import ProductType
+# from .size_type import SizeType
+
+
+# __all__ = [
+#     "LayoutType",
+#     "ProductType",
+#     "SizeType"
+# ]
+__all__ = [
+    "layout_type",
+    "product_type",
+    "size_type"
+]
