783c76cc72c4ffcd45155de78fb059ff8079b92d
vimiomori <vivian.muchen@gmail.com>
2021-01-02 19:46:47 +0900

    use env var for api url

--- src/backendFunctions.ts
+++ src/backendFunctions.ts
@@ -2,7 +2,8 @@ import axios from 'axios'
 import {IProductType} from './types/types'
 
 export const sendQuery = (query: string | undefined, category: IProductType):void => {
-  axios.post('/search', {category: category, query: query})
+  console.log("env", process.env.REACT_APP_API_URL)
+  axios.post(`${process.env.REACT_APP_API_URL}/search`, {category: category, query: query})
   .then(response => {
     console.log('got response for query from backends: \n', response)
     console.log('response data: \n', response.data)
