a3e1a9f3380730d1a7f65cdbba8ba44f16207d8c
scoffrin <scoffrin@indeed.com>
2021-01-06 22:04:22 +0900

    offset width based on last letter elements width in the dom

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -13,28 +13,37 @@ const SearchInput = () => {
     } else {
       setLastLetter('')
     }
+    console.log(lastLetter)
+    console.log(letterWidth)
   }, [inputText])
 
   useEffect(() => {
     if (document.querySelector('.SearchInput__input-letters_last-letter')) {
-      const lastLetterEl = document.querySelector('.SearchInput__input-letters_last-letter') 
       //@ts-ignore
-      setLetterWidth(lastLetterEl.offsetWidth)
+      const lastLetterWidth = document.querySelector('.SearchInput__input-letters_last-letter').offsetWidth
+      if (lastLetterWidth !== 0) {
+        setLetterWidth(lastLetterWidth)
+      } else {
+        setLetterWidth(6)
+      }
     }
-    console.log(lastLetter)
-    console.log(letterWidth)
   }, [lastLetter])
 
   return (
     <div className="SearchInput">
-      <div className="SearchInput__input-letters --text">
-        {inputText.replace(/.$/, '')}
-        <span className="SearchInput__input-letters_last-letter">
-          {lastLetter}
-          <div className={`SearchInput__carot ${lastLetter === '' ? '--inactive' : ''}`}></div>
-        </span>
-      </div>
-      <input 
+      <div className="SearchInput__inner">
+      <div className="testing" contentEditable="true">{inputText}</div>
+        <div className="SearchInput__input-letters --text">
+          {inputText.replace(/.$/, '')}
+          <span className="SearchInput__input-letters_last-letter">
+            {lastLetter}
+            <div 
+              className={`SearchInput__carot ${lastLetter === '' ? '--inactive' : ''}`}
+              style={{left: `${letterWidth + 0.5}px`}}  
+            ></div>
+          </span>
+        </div>
+        <input 
         className="SearchInput__inputEl --text" 
         type="text" 
         placeholder="placeholder or search input component"
@@ -48,6 +57,7 @@ const SearchInput = () => {
         onChange={(event) => setInputText(event.target.value)}
         ref={searchInputEl}
         />
+      </div>
     </div>
   )
 }
