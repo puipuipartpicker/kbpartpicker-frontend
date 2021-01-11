996b3490f26ce0a2725a9c504cd73b685a0380b5
scoffrin <scoffrin@indeed.com>
2021-01-11 23:00:46 +0900

    some logic for handling meta keys

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -5,6 +5,7 @@ const SearchInput = () => {
   const [inputText, setInputText] = useState('')
   const [lastLetter, setLastLetter] = useState('')
   const [carotOffset, setCarotOffset] = useState(0)
+  const [inputLength, setInputLength] = useState(0)
   const searchInputEl = useRef<HTMLInputElement>(null)
 
 
@@ -19,21 +20,50 @@ const SearchInput = () => {
   const handleInputChange = () => {
     console.log(searchInputEl)
     console.log(searchInputEl.current?.selectionStart)
+    if(searchInputEl.current?.value.length) {
+      setInputLength(searchInputEl.current?.value.length)
+    }
     if (searchInputEl.current?.selectionStart) {
-      // 16 is the width of each letter 
       setCarotOffset(searchInputEl.current?.selectionStart)
     }
-    if (searchInputEl.current?.selectionStart === 0) {
-      setCarotOffset(0)
-    }
+    // if (searchInputEl.current?.selectionStart === 0) {
+    //   setCarotOffset(0)
+    // }
   }
 
-  const handleKeypress = () => {
-
+  const handleKeypress = (event:KeyboardEvent) => {
+    if (document.activeElement) {
+      if([...document.activeElement.classList].includes('SearchInput__inputEl')) {
+        console.log(event)
+        console.log('carot offset:', carotOffset)
+        if(event.code === 'ArrowLeft' && !event.metaKey) {
+          if(searchInputEl.current?.value.length && (searchInputEl.current.value.length - 1) >= 0) {
+            setCarotOffset(curOffset => curOffset - 1)
+          }
+        }
+        if(event.code === 'ArrowLeft' && event.metaKey) {
+          setCarotOffset(0)
+        }
+        if(event.code === 'ArrowRight' && !event.metaKey) {
+          if(searchInputEl.current?.value.length) {
+            if(carotOffset + 1 <= searchInputEl.current?.value.length)
+            setCarotOffset(curOffset => curOffset + 1)
+          }
+        }
+        if(event.code === 'ArrowRight' && event.metaKey) {
+          console.log(searchInputEl.current?.selectionEnd)
+          if(searchInputEl.current?.selectionEnd) {
+            console.log('search input length:', searchInputEl.current.value.length)
+            console.log('state input length', inputLength)
+            setCarotOffset(searchInputEl.current.value.length)
+          }
+        }
+      }
+    }
   }
 
   useEffect(() => {
-    document.addEventListener('keydown', () => console.log(searchInputEl))
+    document.addEventListener('keydown', (event) => handleKeypress(event))
   },[])
 
   return (
