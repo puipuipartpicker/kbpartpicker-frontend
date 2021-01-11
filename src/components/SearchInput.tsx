f310721d30f4f1b6349b6bf890238c03fc6dc281
scoffrin <scoffrin@indeed.com>
2021-01-11 20:25:08 +0900

    remove old code

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -16,18 +16,6 @@ const SearchInput = () => {
     }
   }, [inputText])
 
-  // useEffect(() => {
-  //   if (document.querySelector('.SearchInput__input-letters_last-letter')) {
-  //     //@ts-ignore
-  //     const lastLetterWidth = document.querySelector('.SearchInput__input-letters_last-letter').offsetWidth
-  //     if (lastLetterWidth !== 0) {
-  //       setLetterWidth(lastLetterWidth)
-  //     } else {
-  //       setLetterWidth(6)
-  //     }
-  //   }
-  // }, [lastLetter])
-
   const handleInputChange = () => {
     console.log(searchInputEl)
     console.log(searchInputEl.current?.selectionStart)
@@ -40,39 +28,28 @@ const SearchInput = () => {
     }
   }
 
+  const handleKeypress = () => {
+
+  }
+
   useEffect(() => {
-    handleInputChange()
+    document.addEventListener('keydown', () => console.log(searchInputEl))
   },[])
 
   return (
     <div className="SearchInput">
       <div className="SearchInput__inner">
-        {/** TODO: 
-         * consider using contentEditable instead of text input
-          <div className="testing" contentEditable="true">
-          {inputText}
-          <span>Test</span>
-        </div> */}
-        {/* <div className="SearchInput__input-letters --text">
-          {inputText ? inputText.replace(/.$/, '') : 'search here:'}
-          <span className="SearchInput__input-letters_last-letter">
-            {lastLetter}
-            <div 
-              className={`SearchInput__carot ${lastLetter === '' ? '--inactive' : ''}`}
-              style={{left: `${letterWidth + 0.5}px`}}  
-            ></div>
-          </span>
-        </div> */}
         <input 
         className="SearchInput__inputEl --text" 
         type="text" 
         placeholder="placeholder or search input component"
         onChange={() => handleInputChange()}
+        onClick={() => handleInputChange()}
         ref={searchInputEl}
         />
         <div 
           className="SearchInput__carot"
-          style={{left: `${carotOffset ? carotOffset * 15 : 5}px`}}  
+          style={{left: `${carotOffset ? (carotOffset * 14.45555) + 4 : 5}px`}}  
         >
         </div>
       </div>
