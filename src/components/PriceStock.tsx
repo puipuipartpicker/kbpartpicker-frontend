3d1f645f5d22714e22b2f511158eb04b46bc0a86
scootyboots <scott.coffrin@gmail.com>
2021-08-18 21:40:25 +0900

    add PriceStock component

---
+++ src/components/PriceStock.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { ReactComponent as ShoppingCart } from '../svg/icon-shopping-cart.svg'
+import { ReactComponent as Archive } from '../svg/icon-archive.svg'
+import './Product.css'
+
+interface PriceStockProps {
+  price: number
+  stock: boolean
+}
+
+const PriceStock = ({price, stock}:PriceStockProps) => {
+  const stockSvg = stock ? ShoppingCart : Archive
+  return (
+    <div className="PriceStock">
+      {price && <div className="PriceStock-price">${price}</div>}
+      <div className="PriceStock-stock-icon">{stockSvg}</div>
+      <div className="PriceStock-stock-message">{stock ? 'in stock!' : 'out of stock'}</div>
+    </div>
+  )
+}
+
+export default PriceStock
