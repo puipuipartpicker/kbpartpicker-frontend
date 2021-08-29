fa58323ae4574322fe823b51ff8e7115e5275667
scootyboots <scott.coffrin@gmail.com>
2021-08-29 19:15:41 +0900

    remove redundent ulti function

--- src/utils/checkIfImageLoads.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const checkIfImageLoads = async (imgUrl: string) => {
-  const imgRequestResponse = await axios.get(imgUrl).then(resp => true).catch(resp => false)
-  return imgRequestResponse
-}
-
-export default checkIfImageLoads
