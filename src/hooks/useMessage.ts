a7bc98032aa2ea59e8681c96c148b1c2f2ac4940
scootyboots <scott.coffrin@gmail.com>
2021-09-30 18:54:56 +0900

    hook now working

--- src/hooks/useMessage.ts
+++ src/hooks/useMessage.ts
@@ -2,17 +2,9 @@ import { setServers } from "dns";
 import React from "react";
 import { useState } from "react";
 
-const useMessage = (newMessageText?:string, newDisplayMessage?:boolean) => {
+const useMessage = () => {
   const [messageText, setMessageText] = useState('')
   const [displayMessage, setDisplayMessage] = useState(false)
-
-  if (typeof newMessageText === 'string') {
-    setMessageText(newMessageText)
-  }
-  if (typeof newDisplayMessage === 'boolean') {
-    setDisplayMessage(newDisplayMessage)
-  }
-
   return { messageText, setMessageText, displayMessage, setDisplayMessage }
 }
 
